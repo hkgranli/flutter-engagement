ab5b61007bec5b54e9058088f2ed662d13cac65e
Hans Kristian Granli <Hanskristianog@gmail.com>
2023-04-04 09:51:58 +0200

    ios config & read menu

--- lib/read.dart
+++ lib/read.dart
@@ -3,28 +3,80 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ReadPage extends StatelessWidget {
+enum ReadablePages {
+  overview,
+  energy,
+  regulations,
+  social,
+  environmental,
+  economic
+}
+
+class ReadPage extends StatefulWidget {
   const ReadPage({super.key});
+
+  @override
+  State<ReadPage> createState() => _ReadPageState();
+}
+
+class _ReadPageState extends State<ReadPage> {
+
+  var _activePage = 0;
+
+
+  void setPage(int page){
+    setState(() {
+      _activePage = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Scaffold(
-      appBar: CreateAppBar(theme, "Readable"),
-      body: SafeArea(
-          child: Center(
-              child: Column(
-        children: [
-          ReadButton(0, Icons.sunny, "Solar Potential"),
-          ReadButton(0, Icons.storage, "Energy Storage"),
-          ReadButton(0, Icons.account_balance, "Regulations"),
-          ReadButton(0, Icons.people, "Social Sustainability"),
-          ReadButton(0, Icons.money, "Economic Sustainability"),
-          ReadButton(0, Icons.eco, "Environmental Sustainability"),
-        ],
-      ))),
+      appBar: _CreateAppBar(theme),
+      body: _BuildPage(),
       bottomNavigationBar: CreateNavBar(theme, 2, context),
     );
   }
+
+  AppBar _CreateAppBar(ThemeData theme){
+    if(_activePage == 0) return CreateAppBar(theme, "Readable");
+    return CreateAppBar(
+          theme,
+          "Interactive",
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => setPage(0),
+          ));
+  }
+
+  Widget _BuildPage(){
+
+    switch (_activePage){
+      case 0:
+        return _OverviewPage();
+    }
+
+    return Text("error");
+  }
+
+
+  Widget _OverviewPage(){
+      return SafeArea(
+          child: Center(
+              child: Column(
+        children: [
+          ReadButton(1, Icons.sunny, "Solar Potential"),
+          ReadButton(2, Icons.storage, "Energy Storage"),
+          ReadButton(3, Icons.account_balance, "Regulations"),
+          ReadButton(4, Icons.people, "Social Sustainability"),
+          ReadButton(5, Icons.money, "Economic Sustainability"),
+          ReadButton(6, Icons.eco, "Environmental Sustainability"),
+        ],
+      )));
+  }
+
 
   ButtonTheme ReadButton(int i, IconData icon, String text) {
     return ButtonTheme(
